b68b5980899f38329925ec83cdad53d014f8ece6
1910gstv <gustavo.hpaula@outlook.com>
2026-07-01 21:49:06 -0300

    fix: ajustando o retorno ao criar usuário

--- infra/repositories/prismaUserRepository.ts
+++ infra/repositories/prismaUserRepository.ts
@@ -1,18 +1,19 @@
+import { CreateUserDTO } from "../../application/use-cases/UserDTO";
 import { User } from "../../domain/entities/UserEntity";
 import { IUserRepository } from "../../domain/repositories/IUserRepository";
 import { prisma } from "../config/db";
 
 export class PrismaUserRepository implements IUserRepository {
-    public async save(user: any): Promise<User>{
+    public async save(user: CreateUserDTO): Promise<User>{
 
-        await prisma.user.create({data: user});
+        const response = await prisma.user.create({data: user});
 
         return new User(
-            1,
-            'Gustavof',
-            'Henrique',
-            'gustavo@paula.com',
-            '123456'
+            response.id,
+            response.name,
+            response.lastname,
+            response.email,
+            response.password
         )
     }
 }
--- teste.ts
+++ teste.ts
@@ -20,4 +20,4 @@ async function rodarTeste() {
 }
 
 
-rodarTeste();
+// rodarTeste();
